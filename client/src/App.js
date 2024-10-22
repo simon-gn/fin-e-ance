f21f67071ea12dfcc06a29cf939ad2bc54279af5
psycosticks <gerten.simon@web.de>
2024-10-22 21:48:35 +0200

    Deleted seperate Login Page, Login now on HomePage

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,5 @@
-import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
-import LoginPage from './components/LoginPage';
 import RegisterPage from './components/RegisterPage';
 import Dashboard from './components/Dashboard';
 
@@ -10,7 +8,6 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
