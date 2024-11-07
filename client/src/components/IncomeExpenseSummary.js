e8141fb3898e0c10e9edd8ac6669b4f0d84a5955
psycosticks <gerten.simon@web.de>
2024-11-07 17:02:51 +0100

    added some dashboard elements

---
+++ client/src/components/IncomeExpenseSummary.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { calculateMonthlyTotals } from '../utils/transactionUtils';
+import styles from './IncomeExpenseSummary.module.css';
+
+const IncomeExpenseSummary = () => {
+  const { transactions } = useSelector((state) => state.transactions);
+  const { totalIncome, totalExpenses } = calculateMonthlyTotals(transactions);
+
+  const currentDate = new Date();
+  const monthName = currentDate.toLocaleString('default', { month: 'long' });
+
+  return (
+    <div className={`${styles.incomeExpenseSummary} card`}>
+      <h2>{monthName}</h2>
+      <div className={styles.summaryContent}>
+        <div className={`${styles.summaryBox} ${styles.income}`}>
+          <span className={styles.label}>Income</span>
+          <span className={styles.amount}>${totalIncome.toFixed(2)}</span>
+        </div>
+        <div className={`${styles.summaryBox} ${styles.expense}`}>
+          <span className={styles.label}>Expenses</span>
+          <span className={styles.amount}>-${totalExpenses.toFixed(2)}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default IncomeExpenseSummary;
---
+++ client/src/components/TopSpendingCategories.js
@@ -0,0 +1,42 @@
+// components/TopSpendingCategories.js
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { calculateTopSpendingCategories } from '../utils/transactionUtils';
+import styles from './TopSpendingCategories.module.css';
+
+const TopSpendingCategories = () => {
+  const { transactions } = useSelector((state) => state.transactions);
+  const topCategories = calculateTopSpendingCategories(transactions);
+
+	const minLength = 10;
+	const maxLength = 300;
+	
+	let sumTotals = 0;
+  topCategories.forEach(category => {
+		sumTotals += category.total;
+	});
+
+	const calculateBarLength = (total) => {
+		const width = maxLength * total/sumTotals;
+		if (width < minLength)
+			return minLength;
+		return width;
+	}
+
+  return (
+    <div className={`${styles.topSpendingCategories} card`}>
+      <h3>Top Spending Categories</h3>
+      <ul className={styles.categoryList}>
+        {topCategories.map(({ category, total }) => (
+          <li key={category} className={styles.categoryItem}>
+            <span className={styles.categoryName}>{category}</span>
+            <span className={styles.categoryBar} style={{ width: `${calculateBarLength(total)}px` }} />
+            <span className={styles.categoryTotal}>${total.toFixed(2)}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TopSpendingCategories;
