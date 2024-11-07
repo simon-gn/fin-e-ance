99cdb45787be6f87b344d341cd794806f00ccc25
psycosticks <gerten.simon@web.de>
2024-11-07 16:58:25 +0100

    added transactionReducer to the store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import transactionReducer from './reducers/transactionReducer';
 import categoryReducer from './reducers/categoryReducer';
 
 const store = configureStore({
   reducer: {
+    transactions: transactionReducer,
     categories: categoryReducer,
   },
 });
