63db31b3cf01daabe5532f33c3b0c240d5c1ae8b
psycosticks <gerten.simon@web.de>
2024-11-07 16:51:22 +0100

    removed userId requirement from CategorySchema

--- server/src/models/Category.js
+++ server/src/models/Category.js
@@ -7,7 +7,6 @@ const CategorySchema = new mongoose.Schema(
     userId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
-      required: true,
     },
   },
   { timestamps: true },
