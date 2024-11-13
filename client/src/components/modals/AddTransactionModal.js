d481664b32522bd71d2d605f6b92a3f519cae575
psycosticks <gerten.simon@web.de>
2024-11-13 20:40:35 +0100

    changed layout of modal from form to pop up

--- client/src/components/modals/AddTransactionModal.js
+++ client/src/components/modals/AddTransactionModal.js
@@ -19,71 +19,73 @@ const AddTransactionModal = ({ isOpen, onClose }) => {
 
   return (
     isOpen && (
-      <form
-        className={styles.addTransactionForm}
-        onSubmit={(e) => {
-          e.preventDefault();
-          handleSave();
-        }}
-      >
-        <div>
-          <label htmlFor="type">Type:</label>
-          <select
-            id="type"
-            name="type"
-            value={type}
-            onChange={(e) => setType(e.target.value)}
-            required
-          >
-            <option value="Income">Income</option>
-            <option value="Expense">Expense</option>
-          </select>
-        </div>
-        <div>
-          <label htmlFor="category">Category:</label>
-          <select
-            id="category"
-            name="category"
-            value={category}
-            onChange={(e) => setCategory(e.target.value)}
-            required
-          >
-            <option value="" disabled>
-              Select a category
-            </option>
-            {categories.map((cat) => (
-              <option key={cat._id} value={cat._id}>
-                {cat.name}
+      <div className={styles.modalOverlay}>
+        <form
+          className={styles.addTransactionForm}
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleSave();
+          }}
+        >
+          <div>
+            <label htmlFor="type">Type:</label>
+            <select
+              id="type"
+              name="type"
+              value={type}
+              onChange={(e) => setType(e.target.value)}
+              required
+            >
+              <option value="Income">Income</option>
+              <option value="Expense">Expense</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="category">Category:</label>
+            <select
+              id="category"
+              name="category"
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+              required
+            >
+              <option value="" disabled>
+                Select a category
               </option>
-            ))}
-          </select>
-        </div>
-        <div>
-          <label htmlFor="amount">Amount:</label>
-          <input
-            id="amount"
-            name="amount"
-            type="number"
-            value={amount}
-            onChange={(e) => setAmount(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="description">Description:</label>
-          <input
-            id="description"
-            name="description"
-            type="text"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-          />
-        </div>
-        <button type="submit">Add Transaction</button>
-        <button type="button" onClick={() => onClose()}>
-          Cancel
-        </button>
-      </form>
+              {categories.map((cat) => (
+                <option key={cat._id} value={cat._id}>
+                  {cat.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <label htmlFor="amount">Amount:</label>
+            <input
+              id="amount"
+              name="amount"
+              type="number"
+              value={amount}
+              onChange={(e) => setAmount(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="description">Description:</label>
+            <input
+              id="description"
+              name="description"
+              type="text"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          </div>
+          <button type="submit">Add Transaction</button>
+          <button type="button" onClick={onClose}>
+            Cancel
+          </button>
+        </form>
+      </div>
     )
   );
 };
