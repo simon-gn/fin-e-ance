3d3d78ec43d63fccab8f30da7a13b645b30f60eb
psycosticks <gerten.simon@web.de>
2024-11-13 20:37:40 +0100

    moved calculation of category expenses and improved styling

--- client/src/components/charts/CategoryBreakdownChart.js
+++ client/src/components/charts/CategoryBreakdownChart.js
@@ -1,43 +1,58 @@
 import React from "react";
 import { ResponsivePie } from "@nivo/pie";
-import { useSelector } from "react-redux";
+import { calculateExpensesByCategory } from "../../utils/transactionUtils";
 
-const CategoryBreakdownChart = () => {
-  const { transactions } = useSelector((state) => state.transactions);
+const CategoryBreakdownChart = ({transactions}) => {
+  const expensesByCategory = calculateExpensesByCategory(transactions, 15);
 
-  const expenses = transactions.filter(
-    (transaction) => transaction.type === "Expense",
-  );
-
-  // Calculate total per category
-  const categoryTotals = expenses.reduce((acc, transaction) => {
-    const category = transaction.category.name;
-    acc[category] = (acc[category] || 0) + transaction.amount;
-    return acc;
-  }, {});
-
-  const data = Object.entries(categoryTotals).map(([category, amount]) => ({
+  const data = expensesByCategory.map(({ category, total }) => ({
     id: category,
     label: category,
-    value: amount,
+    value: total,
   }));
 
   return (
-    <div style={{ height: "500px" }}>
+    <div style={{ height: 250 }}>
       <ResponsivePie
         data={data}
-        margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
-        innerRadius={0.5}
-        padAngle={1}
+        margin={{ top: 30, right: 30, bottom: 30, left: -80 }}
+        innerRadius={0.7}
+        padAngle={0.5}
         cornerRadius={3}
-        colors={{ scheme: "pastel1" }}
-        borderWidth={1}
-        borderColor={{ from: "color", modifiers: [["darker", 0.2]] }}
-        radialLabel={(d) => `${d.id} (${d.value})`}
-        sliceLabel={(d) => `${d.value}`}
-        slicesLabelsSkipAngle={10}
-        radialLabelsLinkColor={{ from: "color" }}
-        radialLabelsTextColor="#333333"
+        // colors={{ scheme: 'nivo' }}
+        colors={['#37c3c8', '#ffc542', '#ff575f', '#ba68c8']}
+        enableArcLinkLabels={false}
+        radialLabelsSkipAngle={10}
+        enableSlicesLabels={false}
+        animate={true}
+        theme={{
+          labels: { text: { fontWeight: "var(--text_color)" } },
+        }}
+        legends={[
+            {
+                anchor: 'right',
+                direction: 'column',
+                justify: false,
+                translateX: -40,
+                translateY: 0,
+                itemsSpacing: 18,
+                itemWidth: 0,
+                itemHeight: 18,
+                itemTextColor: "var(--text_color)",
+                itemDirection: 'left-to-right',
+                itemOpacity: 1,
+                symbolSize: 14,
+                symbolShape: 'circle',
+                effects: [
+                    {
+                        on: 'hover',
+                        style: {
+                            itemTextColor: '#000'
+                        }
+                    }
+                ]
+            }
+        ]}
       />
     </div>
   );
