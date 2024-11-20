45c8000969618c46cf62f187f3d47bad0733c097
psycosticks <gerten.simon@web.de>
2024-11-21 00:13:59 +0100

    reordered category colors

--- client/src/styles/theme.js
+++ client/src/styles/theme.js
@@ -1,9 +1,9 @@
 export const COLORS_CATEGORIES = [
-  "#37c3c8", // green
-  "#ffc542", // yellow
   "#ff575f", // red
-  "#ba68c8", // violett
   "#fe8427", // orange
+  "#ffc542", // yellow
+  "#37c3c8", // green
   "#0056f8", // blue
+  "#ba68c8", // violett
   "#9e9e9e", // grey
 ];
