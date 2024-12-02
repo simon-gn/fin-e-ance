f1be8b127a22e31305f40c1f7e72fe092127ebcc
psycosticks <gerten.simon@web.de>
2024-12-02 19:50:13 +0100

    moved header

--- client/src/components/charts/IncomeExpenseComparisonChart.js
+++ client/src/components/charts/IncomeExpenseComparisonChart.js
@@ -21,83 +21,86 @@ const IncomeExpenseComparisonChart = () => {
   });
 
   return (
-    <div style={{ height: "500px" }}>
-      <ResponsiveBar
-        data={chartData}
-        keys={["Income", "Expense"]}
-        indexBy="month"
-        margin={{ top: 20, right: 90, bottom: 70, left: 60 }}
-        padding={0.3}
-        // colors={{ scheme: 'pastel2' }}
-        colors={({ id }) =>
-          id === "Income" ? "var(--income_color)" : "var(--expense_color)"
-        }
-        colorBy="id"
-        borderColor={{ from: "color", modifiers: [["darker", 1.6]] }}
-        axisBottom={{
-          tickSize: 5,
-          tickPadding: 5,
-          tickRotation: -45,
-          legend: "Month",
-          legendPosition: "middle",
-          legendOffset: 60,
-        }}
-        axisLeft={{
-          tickSize: 5,
-          tickPadding: 5,
-          tickRotation: 0,
-          legend: "Amount ($)",
-          legendPosition: "middle",
-          legendOffset: -50,
-        }}
-        labelSkipWidth={12}
-        labelSkipHeight={12}
-        labelTextColor={{ from: "color", modifiers: [["darker", 1.6]] }}
-        legends={[
-          {
-            dataFrom: "keys",
-            anchor: "bottom-right",
-            direction: "column",
-            justify: false,
-            translateX: 120,
-            translateY: 0,
-            itemsSpacing: 2,
-            itemWidth: 100,
-            itemHeight: 20,
-            itemTextColor: "var(--text_color)",
-            itemDirection: "left-to-right",
-            itemOpacity: 0.85,
-            symbolSize: 20,
-            effects: [
-              {
-                on: "hover",
-                style: {
-                  itemOpacity: 1,
+    <div className="card">
+      <h3>Income vs. Expense</h3>
+      <div style={{ height: "500px" }}>
+        <ResponsiveBar
+          data={chartData}
+          keys={["Income", "Expense"]}
+          indexBy="month"
+          margin={{ top: 20, right: 90, bottom: 70, left: 60 }}
+          padding={0.3}
+          // colors={{ scheme: 'pastel2' }}
+          colors={({ id }) =>
+            id === "Income" ? "var(--income_color)" : "var(--expense_color)"
+          }
+          colorBy="id"
+          borderColor={{ from: "color", modifiers: [["darker", 1.6]] }}
+          axisBottom={{
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: -45,
+            legend: "Month",
+            legendPosition: "middle",
+            legendOffset: 60,
+          }}
+          axisLeft={{
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: "Amount ($)",
+            legendPosition: "middle",
+            legendOffset: -50,
+          }}
+          labelSkipWidth={12}
+          labelSkipHeight={12}
+          labelTextColor={{ from: "color", modifiers: [["darker", 1.6]] }}
+          legends={[
+            {
+              dataFrom: "keys",
+              anchor: "bottom-right",
+              direction: "column",
+              justify: false,
+              translateX: 120,
+              translateY: 0,
+              itemsSpacing: 2,
+              itemWidth: 100,
+              itemHeight: 20,
+              itemTextColor: "var(--text_color)",
+              itemDirection: "left-to-right",
+              itemOpacity: 0.85,
+              symbolSize: 20,
+              effects: [
+                {
+                  on: "hover",
+                  style: {
+                    itemOpacity: 1,
+                  },
+                },
+              ],
+            },
+          ]}
+          theme={{
+            grid: {
+              line: {
+                stroke: "var(--text_color)",
+              },
+            },
+            axis: {
+              ticks: {
+                text: {
+                  fill: "var(--text_color)",
                 },
               },
-            ],
-          },
-        ]}
-        theme={{
-          grid: {
-            line: {
-              stroke: "var(--text_color)",
-            },
-          },
-          axis: {
-            ticks: {
-              text: {
-                fill: "var(--text_color)",
+              legend: {
+                text: {
+                  fill: "var(--text_color)",
+                },
               },
             },
-            legend: {
-              text: {
-                fill: "var(--text_color)",
-              },
-            },
-          },
-        }}
-      />
+          }}
+        />
+      </div>
     </div>
   );
 };
