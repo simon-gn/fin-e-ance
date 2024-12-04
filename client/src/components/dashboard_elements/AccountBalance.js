1ced1c3b54266654975c80704e94a76da5fb715b
psycosticks <gerten.simon@web.de>
2024-12-04 12:43:17 +0100

    fixed small bug when no account balance data is present

--- client/src/components/dashboard_elements/AccountBalance.js
+++ client/src/components/dashboard_elements/AccountBalance.js
@@ -3,47 +3,53 @@ import { ResponsiveLine } from "@nivo/line";
 import styles from "./AccountBalance.module.css";
 
 const AccountBalance = ({ accountBalances }) => {
-  const chartData = [
-    {
-      id: "AccountBalance",
-      data: accountBalances.map((balance) => ({
-        x: balance.date,
-        y: balance.amount,
-      })),
-    },
-  ];
+  const hasData = accountBalances && accountBalances.length > 0;
+
+  const chartData = hasData
+    ? [
+        {
+          id: "AccountBalance",
+          data: accountBalances.map((balance) => ({
+            x: balance.date,
+            y: balance.amount,
+          })),
+        },
+      ]
+    : [];
 
   return (
     <div className="card">
       <div className={styles.accountBalance}>
         <h3>Account Balance</h3>
         <span className={styles.amount}>
-          ${accountBalances[0] ? accountBalances[0].amount.toFixed(2) : null}
+          ${hasData ? accountBalances[0].amount.toFixed(2) : null}
         </span>
 
         <div style={{ width: "100%", height: "80px" }}>
-          <ResponsiveLine
-            data={chartData}
-            margin={{ top: 15, right: 0, bottom: 10, left: 0 }}
-            xScale={{ type: "point" }}
-            yScale={{
-              type: "linear",
-              min: "auto",
-              max: "auto",
-              stacked: true,
-              reverse: false,
-            }}
-            curve="natural"
-            axisTop={null}
-            axisRight={null}
-            axisBottom={null}
-            axisLeft={null}
-            enableGridX={false}
-            enableGridY={false}
-            enablePoints={false}
-            isInteractive={false}
-            legends={[]}
-          />
+          {hasData && (
+            <ResponsiveLine
+              data={chartData}
+              margin={{ top: 15, right: 0, bottom: 10, left: 0 }}
+              xScale={{ type: "point" }}
+              yScale={{
+                type: "linear",
+                min: "auto",
+                max: "auto",
+                stacked: true,
+                reverse: false,
+              }}
+              curve="natural"
+              axisTop={null}
+              axisRight={null}
+              axisBottom={null}
+              axisLeft={null}
+              enableGridX={false}
+              enableGridY={false}
+              enablePoints={false}
+              isInteractive={false}
+              legends={[]}
+            />
+          )}
         </div>
       </div>
     </div>
